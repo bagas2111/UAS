44c5bd96a40fbbda1ac523ade7f0c02856f5a60c
bagas2111 <64072144+bagas2111@users.noreply.github.com>
2022-11-25 20:24:06 +0700

    Add files via upload

---
+++ 1UAS/fasilitas.php
@@ -0,0 +1,39 @@
+<?php
+include('config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="a.css">
+    <title>index</title>
+</head>
+<body>
+    <div class="box">
+        <h2><a href="" style="text-decoration:none ;">Hotel Hebat</a></h2>
+        <div class="menu">
+            <a href="logout.php"><img src="Logout.png" alt=""></a>
+            <a href="#">fasilitas</a>
+            <a href="#">Kamar</a>
+            <a href="#">Home</a>
+            
+        </div>
+        <div class="banner"></div>
+        <div class="konten">
+            <h3>FASILITAS</h3>
+            <table>
+                <tr>
+                    <td><img src="https://www.herworld.co.id/gallery/teaser/b32b2801ad3c30762ee449a858b5b16a.jpg" alt=""></td>
+                    <td><img src="https://cdn-2.tstatic.net/travel/foto/bank/images/fasilitas-kolam-renang-di-the-jayakarta-cisarua-inns.jpg" alt=""></td>
+                    <td><img src=""></td>
+                </tr>
+            </table>
+            
+        </div>
+    </div>
+    
+
+</body>
+</html>
